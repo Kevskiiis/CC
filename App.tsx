c3650c27a6b8dbbbfca77e4838962f945c0c9980
Kevin R. <delword07@gmail.com>
2025-09-03 22:04:28 -0700

    Created Opening Screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import OpeningScreen from './src/screens/OpeningScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <OpeningScreen />
+      {/* <OpeningScreen /> */}
+      <LoginScreen/> 
     </View>
+  
   );
 }
 
--- src/screens/OpeningScreen.tsx
+++ src/screens/OpeningScreen.tsx
@@ -1,25 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function OpeningScreen() {
   return (
-    <View style={styles.container}>
+    <View style={OpeninScreenStyle.container}>
         <Image 
             source={{uri:'https://tse1.mm.bing.net/th/id/OIP.fssi_WH8r1e_i5oXsAmKYwHaEQ?cb=thfvnext&rs=1&pid=ImgDetMain&o=7&rm=3'}}
-                    style={{width: 150, height: 150}}
+                    style={OpeninScreenStyle.logo}
         ></Image>
-        <Button title='Login'></Button>
-        <Button title='Create Account'></Button>
+        {/* Login Button */}
+        <TouchableOpacity style={OpeninScreenStyle.button}>
+          <Text style={OpeninScreenStyle.buttonText}>Login</Text>
+        </TouchableOpacity>
+        {/* Create Account Button */}
+        <TouchableOpacity style={OpeninScreenStyle.button}>
+          <Text style={OpeninScreenStyle.buttonText}>Create Account</Text>
+        </TouchableOpacity>
+
         <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const OpeninScreenStyle = StyleSheet.create({ 
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: '#4CAF50',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 150,
+    height: 150
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    borderWidth: 5,
+    borderColor: '#000000ff',
+    width: 220,
+    height: 60,
+    margin: 5,
+    backgroundColor: '#F5F5F5'
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: '500',
+    fontFamily: ''
+  }
 });
