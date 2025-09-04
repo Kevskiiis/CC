accfdf126ecdb7cc9e757e8a0c5a0ef450a7ef04
Kevin R. <delword07@gmail.com>
2025-09-03 22:04:46 -0700

    Initialized Login Screen

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,19 @@
+import {View, StyleSheet} from 'react-native'; 
+
+function LoginScreen () {
+    return (
+        <View style={LoginStyle.container}>
+            
+        </View>
+    )
+}
+export default LoginScreen;
+
+
+const LoginStyle = StyleSheet.create ({
+    container: {
+        flex: 1,
+    },
+
+    
+})
