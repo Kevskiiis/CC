29cffeea2f80263e5d3356e4b436b4046a432568
Kevin R. <delword07@gmail.com>
2025-09-28 00:09:31 -0700

    Finalized Create Account Screen Draft

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,195 +1,166 @@
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, Keyboard} from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, Keyboard} from "react-native";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import PhoneInput from 'react-native-phone-input';
 import { useState, useRef } from "react";
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { OpeningRoutesStackParams } from '../routes/OpeningRoutes';
 
-//
-import { responsive } from "../utils/responsive";
+// React Native Paper Library Components:
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { TextInput, Appbar, Button, Icon} from 'react-native-paper';
+import TextInputMask from "react-native-text-input-mask";
 
-// Components:
-import InputBox from "../components/InputBox";
+// Responsive Utility for Styling:
+import { responsive } from "../utils/responsive";
+import AppbarBackAction from "react-native-paper/lib/typescript/components/Appbar/AppbarBackAction";
 
 type props = NativeStackScreenProps<OpeningRoutesStackParams, 'RegisterScreen'>;
 
 function RegisterScreen ({navigation}: props) {
     // References:
-    const firstName = useRef<TextInput>(null);
-    const lastName = useRef<TextInput>(null);
-    const phoneNumber = useRef<PhoneInput>(null);
-    const email = useRef<TextInput>(null);
-    const password = useRef<TextInput>(null);
+    // const firstName = useRef<TextInput>(null);
+    // const lastName = useRef<TextInput>(null);
+    // const phoneNumber = useRef<PhoneInput>(null);
+    // const email = useRef<TextInput>(null);
+    // const password = useRef<TextInput>(null);
 
+    // States:
     const [newForm, setForm] = useState({
 
     })
 
-    function handleEditChange (object: React.RefObject<TextInput | PhoneInput>) {
-        object.current?.focus()
-    }   
+    // function handleEditChange (object: React.RefObject<TextInput | PhoneInput>) {
+    //     object.current?.focus()
+    // }   
 
     return (
-        <KeyboardAwareScrollView
-            style={RegisterStyles.scrollViewOutside}
-            contentContainerStyle={RegisterStyles.scrollViewInside}
-            enableOnAndroid={true}
-            extraScrollHeight={responsive.number(100)}
-            enableAutomaticScroll={true}
-            keyboardOpeningTime={350}
-        >
-            <View style={RegisterStyles.formContainer}>
-                {/* Back Button */}
-                <View style={RegisterStyles.backButtonContainer}>
-                    <TouchableOpacity style={RegisterStyles.backButton} onPress={() => navigation.goBack()}>
-                        <Text style={RegisterStyles.buttonText}>Back</Text>
-                    </TouchableOpacity>
-                </View>
-                {/* New User Entries */}
-                <View style={RegisterStyles.inputsContainer}>
-                    <Text style={RegisterStyles.label}>First Name</Text>
-                    <TextInput ref={firstName} onSubmitEditing={() => {
-                        lastName.current?.focus();
-                        Keyboard.isVisible();
-                    }} style={RegisterStyles.textInput} placeholder="First Name" />
-
-                    <Text style={RegisterStyles.label}>Last Name</Text>
-                    <TextInput ref={lastName} style={RegisterStyles.textInput} placeholder="Last Name" />
-
-                    <Text style={RegisterStyles.label}>Phone Number</Text>
-                    <PhoneInput
-                        ref={phoneNumber}
-                        initialCountry="us"
-                        autoFormat={true}
-                        style={RegisterStyles.phoneInput}
-                        textStyle={{ fontSize: responsive.number(16) }}
-                        flagStyle={{ width: responsive.number(30), height: responsive.number(20)}}
-                    />
-
-                    <Text style={RegisterStyles.label}>Email</Text>
-                    <TextInput ref={email} style={RegisterStyles.textInput} placeholder="Email" />
-
-                    <Text style={RegisterStyles.label}>Password</Text>
-                    <TextInput ref={password} style={RegisterStyles.textInput} secureTextEntry={true} placeholder="Password" onSubmitEditing={() => {
-                        Keyboard.dismiss();
-                    }}/>
-
-                    <TouchableOpacity style={RegisterStyles.button}>
-                        <Text style={RegisterStyles.buttonText}>Create Account</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </KeyboardAwareScrollView>
+        <SafeAreaProvider>
+            <SafeAreaView style={RegisterStyles.screen}>
+                <KeyboardAwareScrollView
+                    // style={RegisterStyles.scrollViewOutside}
+                    contentContainerStyle={RegisterStyles.scrollViewInside}
+                    enableOnAndroid={true}
+                    extraScrollHeight={responsive.number(80)}
+                    enableAutomaticScroll={true}
+                >
+                    <View style={RegisterStyles.formContainer}>
+                        <Appbar.Header style={RegisterStyles.appBar}>
+                            <Appbar.Content titleStyle={RegisterStyles.appBarTitle} title="Create Account" color="#DDA853"/>
+                            <Appbar.Action style={RegisterStyles.appBarItem} icon="arrow-left" color="#1a1a1aff" onPress={() => navigation.goBack()} />
+                        </Appbar.Header>
+                        <TextInput 
+                            style={RegisterStyles.textInput} 
+                            label="First Name" 
+                            placeholder="John" 
+                            error={false} 
+                            mode="flat"
+                            underlineColor="#1a1a1aff"
+                            activeUnderlineColor="#1a1a1aff"
+                        >
+                        </TextInput>
+                        <TextInput style={RegisterStyles.textInput} label="Last Name" placeholder="White" error={false} mode="flat"></TextInput>
+                        <PhoneInput
+                            // ref={phoneNumber}
+                            initialCountry="us"
+                            autoFormat={true}
+                            style={RegisterStyles.phoneInput}
+                            textStyle={{ fontSize: responsive.number(16) }}
+                            flagStyle={{ width: responsive.number(30), height: responsive.number(20)}}
+                            // onPressFlag={() => {
+                            //     console.log('Implement this later.')
+                            // }}
+                            // onPressConfirm={() =>{
+                            //     email.current?.focus();
+                            // }}
+                        />
+                        <TextInput style={RegisterStyles.textInput} label="Email" placeholder="example@gmail.com" error={false} mode="flat"></TextInput>
+                        <TextInput style={RegisterStyles.textInput} label="Password" placeholder="Example14!" error={false} mode="flat"></TextInput>
+                        <Button style={RegisterStyles.submitButton} textColor="#DDA853">SIGN UP</Button>
+                    </View>
+                </KeyboardAwareScrollView>
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
 export default RegisterScreen;
 
 const RegisterStyles = StyleSheet.create({
-    buttonText: {
-        color: '#ffffff',
-        fontWeight: '700',
-        fontSize: 20
+    screen: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#D2C1B6'
     },
-    button: {
-        flexDirection: 'row',
+    submitButton: {
+        alignItems: 'center',
         justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: responsive.number(4),
-        borderRadius: responsive.number(10),
-        marginTop: responsive.number(10),
-        height: responsive.number(50),
-        width: responsive.number(275),
-        backgroundColor: '#000000ff'
+        width: responsive.number(300),
+        height: responsive.number(60),
+        borderRadius: responsive.number(4),
+        borderWidth: responsive.number(2),
+        backgroundColor: '#1a1a1aff'
     },
-    backButton: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: responsive.number(4),
-        borderRadius: responsive.number(15),
-        width: responsive.number(100),
-        height: responsive.number(50),
-        backgroundColor: '#000000ff'
-    },
-    label: {
+    textInput: {
         flexDirection: 'row',
-        justifyContent: 'flex-start',
+        width: responsive.number(300),
+        height: responsive.number(60),
+        backgroundColor: '#ecececff'
+    },
+    appBar: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        paddingLeft: responsive.number(8),
-        fontSize: responsive.fontSize(18),
-        // fontFamily: 'Georgia',
-        fontWeight: "600",
-        height: responsive.number(30),
         width: '100%',
-        // backgroundColor: '#4CAF50'
+        height: responsive.number(80),
+        backgroundColor: '#1a1a1aff',
+        // tintColor: '#fcfcfcff'
     },
-    scrollViewOutside: {
+    appBarTitle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: '300',
+        fontSize: responsive.number(22)
+    },
+    appBarItem: {
+        width: responsive.number(100),
+        height: responsive.number(60),
+        color: '#ffffffff',
+        // fontWeight: '700',
+        backgroundColor: '#DDA853'
     },
     scrollViewInside: {
         flexGrow: 1,
         flexDirection: 'column',
-        justifyContent: 'flex-start', 
+        justifyContent: 'center', 
         alignItems: 'center',
-        paddingTop: responsive.number(50),
-        paddingBottom: responsive.number(30),
-        backgroundColor: '#4087d8ff'
+        gap: responsive.number(20),
+        backgroundColor: '#D2C1B6'
     },
     formContainer: {
-        // flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'stretch',
-        gap: responsive.number(15),
-        paddingTop: responsive.number(10),
-        paddingLeft: responsive.number(5),
-        paddingRight: responsive.number(5),
-        width: responsive.number(340),
-        height: responsive.number(700),
-        borderWidth: responsive.number(6),
-        borderRadius: responsive.number(8),
-        backgroundColor: '#D0F0C0',
-    },
-    backButtonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        width:'100%',
-        height: responsive.number(60),
-        // backgroundColor: '#ffffff'
-    },
-    inputsContainer: {
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        gap: responsive.number(10),
-        paddingLeft: responsive.number(10),
-        paddingRight: responsive.number(10),
-        width: '100%',
-        height: responsive.number(600),
-        // backgroundColor: '#ba3636ff'
+        gap: responsive.number(15),
+        paddingBottom: responsive.number(40),
+        width: '90%',
+        height: 'auto',
+        borderWidth: responsive.number(6),
+        borderRadius: responsive.number(8),
+        borderColor: '#1a1a1aff',
+        backgroundColor: '#F5F5F0',
     },
     phoneInput: {
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
         paddingLeft: 10,
-        borderRadius: responsive.number(10),
-        borderWidth: responsive.number(4),
+        borderTopRightRadius: responsive.number(4),
+        borderTopLeftRadius: responsive.number(4),
+        borderBottomWidth: responsive.number(1/2),
+        borderColor: '#1a1a1aff',
         fontSize: responsive.number(16),
-        height: responsive.number(50),
-        width: '100%',
-        color: '#000000ff',
-        backgroundColor: '#ffffff'
+        width: responsive.number(300),
+        height: responsive.number(60),
+        color: '#1a1a1aff',
+        backgroundColor: '#ecececff'
     },
-    textInput: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        paddingLeft: 10,
-        borderRadius: responsive.number(10),
-        borderWidth: responsive.number(4),
-        fontSize: responsive.number(16),
-        height: responsive.number(50),
-        width: '100%',
-        color: '#000000ff',
-        backgroundColor: '#ffffff'
-    }
 }) 
