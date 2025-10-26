7648c2772f90c0f44fbb268b71061c902a60ff9f
Kevin R. <delword07@gmail.com>
2025-10-25 19:42:20 -0700

    Modified code to fit the new database update

--- src/screens/PrivateScreens/ProfileScreen.tsx
+++ src/screens/PrivateScreens/ProfileScreen.tsx
@@ -12,11 +12,12 @@ export default function ProfileScreen () {
                 <Text>User's </Text>
                 <IconButton icon="cog" mode="contained" size={30}/>
             </View>
-            {/* <Avatar.Image size={20} src={require('')}/> */}
-            <Text>Profile Name</Text>
-            <Text>Bio is here, who am I</Text>
-            <Divider/>
-            <Button style={ProfileScreenStyles.button}>Edit Profile</Button>
+            <View style={ProfileScreenStyles.configurationContainer}>
+                <Avatar.Image style={ProfileScreenStyles.avatar} size={responsive.number(100)} source={require('../../../assets/icon.png')}/>
+                <Text style={ProfileScreenStyles.profileName}>Kevin Rodriguez</Text>
+                <Text style={ProfileScreenStyles.bio}>I am a pro Fortnite player. I know the ganme is dead, but I'm still the best.</Text>
+                <Button style={ProfileScreenStyles.button}>Edit Profile</Button>
+            </View>
             <Divider/>
             <Card style={ProfileScreenStyles.card} onPress={() => console.log(' was pressed')}>
                 <Card.Title title="Create Post"/>
@@ -38,8 +39,23 @@ const ProfileScreenStyles = StyleSheet.create({
         backgroundColor: COLORS.primary
     },
     configurationContainer: {
-        flexDirection: 'row',
-        gap: responsive.number(20)
+        gap: responsive.number(10),
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    profileName: {
+        fontSize: responsive.fontSize(20)
+    },
+    bio: {
+        alignSelf: 'flex-start',
+        paddingLeft: responsive.number(15),
+        paddingRight: responsive.number(15),
+        paddingTop: responsive.number(10),
+        paddingBottom: responsive.number(30),
+        fontSize: responsive.fontSize(14)
+    },
+    avatar: {
+        // borderWidth: responsive.number(2)
     },
     card: {
         width: responsive.number(230),
